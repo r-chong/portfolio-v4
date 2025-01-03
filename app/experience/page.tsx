6ad548308103dc54857fddf4d99eb40c0e8d3a3f
Reese Chong <dev.reese.chong@gmail.com>
2025-01-03 05:20:52 -0500

    feat: add resume embed

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import Experience from '../../components/Experience';
 import { HiDownload } from 'react-icons/hi';
 
 export default function ExperiencePage() {
     return (
-        <div>
+        <div className='flex flex-col items-center justify-center min-h-screen p-4'>
+            <div className='w-full max-w-4xl bg-white shadow-md rounded-lg overflow-hidden'>
+                <iframe
+                    src='https://drive.google.com/file/d/1dpfR7P8Zod_9FjLaqR8FIglcsLAocHYo/preview'
+                    className='w-full h-[50vh] sm:h-[80vh]'
+                    title='Experience Document'
+                />
+            </div>
             <a
-                className='flex items-center gap-2 py-3 text-white transition bg-gray-900 rounded-full outline-none group px-7 focus:scale-110 hover:scale-110 active:scale-105 border-black/10'
-                href='/Reese_Chong_Resume.pdf'
-                download
+                className='mt-6 flex items-center px-4 py-2 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
+                href='https://drive.google.com/file/d/1dpfR7P8Zod_9FjLaqR8FIglcsLAocHYo/view?usp=sharing'
+                target='_blank'
             >
-                Download CV{' '}
-                <HiDownload className='transition opacity-60 group group-hover:translate-y-1' />{' '}
-                {''}
+                <HiDownload className='mr-2 w-5 h-5' /> Download
             </a>
         </div>
     );
