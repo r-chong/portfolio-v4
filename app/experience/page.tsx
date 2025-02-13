b1b589141da364094aaf2c0f6daf5f014589ddd5
Reese Chong <dev.reese.chong@gmail.com>
2025-02-13 18:58:36 -0500

    chore: redirect experience to resume

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,30 +1,14 @@
-import React from 'react';
-import { HiDownload } from 'react-icons/hi';
+'use server';
 
-export default function ExperiencePage() {
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
+export default async function experienceRedirect(id: string) {
+    redirect(`/resume`);
+
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen p-4'>
-            <div className='w-full max-w-4xl bg-white shadow-md rounded-lg overflow-hidden'>
-                {process.env.NEXT_PUBLIC_RESUME_LINK ? (
-                    <>
-                        {' '}
-                        <iframe
-                            src={`${process.env.NEXT_PUBLIC_RESUME_LINK}`}
-                            className='w-full h-[50vh] sm:h-[80vh]'
-                            title='Experience Document'
-                        />
-                        <a
-                            className='mt-6 flex items-center px-4 py-2 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
-                            href={`${process.env.NEXT_PUBLIC_RESUME_LINK}`}
-                            target='_blank'
-                        >
-                            <HiDownload className='mr-2 w-5 h-5' /> Download
-                        </a>
-                    </>
-                ) : (
-                    ''
-                )}
-            </div>
+        <div>
+            That&apos;s weird, you should have been redirected to my resume
         </div>
     );
 }
---
+++ app/resume/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { HiDownload } from 'react-icons/hi';
+
+export default function ResumePage() {
+    return (
+        <div className='flex flex-col items-center justify-center min-h-screen p-4'>
+            <div className='w-full max-w-4xl bg-white shadow-md rounded-lg overflow-hidden'>
+                {process.env.NEXT_PUBLIC_RESUME_LINK ? (
+                    <>
+                        {' '}
+                        <iframe
+                            src={`${process.env.NEXT_PUBLIC_RESUME_LINK}`}
+                            className='w-full h-[50vh] sm:h-[80vh]'
+                            title='Experience Document'
+                        />
+                        <a
+                            className='mt-6 flex items-center px-4 py-2 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
+                            href={`${process.env.NEXT_PUBLIC_RESUME_LINK}`}
+                            target='_blank'
+                        >
+                            <HiDownload className='mr-2 w-5 h-5' /> Download
+                        </a>
+                    </>
+                ) : (
+                    ''
+                )}
+            </div>
+        </div>
+    );
+}
