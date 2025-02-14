715b66068adf200b98d42567e2ee026feb093226
Reese Chong <dev.reese.chong@gmail.com>
2025-02-13 19:23:01 -0500

    refactor: change redirection method

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects() {
+        return [
+            {
+                source: '/experience',
+                destination: '/resume',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 module.exports = withContentlayer(nextConfig);
