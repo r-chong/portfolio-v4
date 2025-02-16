2b251f6fcf4d40abc8114dbc2348bc864aa64feb
Reese Chong <dev.reese.chong@gmail.com>
2025-02-16 06:27:29 -0500

    style: add indicator for current page

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,17 +4,43 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link, { LinkProps } from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const pathname = usePathname();
 
     return (
         <header className='relative z-[999]'>
-            <motion.div
-                className='fixed top-0 left-1/2 h-[4.5rem] hidden sm:block w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-xl'
-                initial={{ y: -100, x: '-50%', opacity: 0 }}
-                animate={{ y: 0, x: '-50%', opacity: 1 }}
-            ></motion.div>
+            <div className='fixed top-0 left-1/2 h-[4.5rem] hidden sm:block w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-xl -translate-x-1/2' />
+
+            {/* Desktop Navigation */}
+            <nav className='hidden sm:flex fixed top-[1.7rem] left-1/2 h-12 -translate-x-1/2 py-2'>
+                <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:flex-nowrap sm:gap-5'>
+                    {links.map((link) => {
+                        const isActive = link.url === pathname;
+
+                        return (
+                            <li
+                                key={link.hash}
+                                className='relative flex items-center justify-center h-3/4'
+                            >
+                                {isActive && (
+                                    <div className='absolute left-[-0.75rem] right-[-0.5rem] top-[-0.5rem] bottom-[-0.5rem] bg-gradient-to-b from-white/80 to-white/50 backdrop-blur-[0.5rem] shadow-[inset_0_-1px_1px_rgba(0,0,0,0.02),0_1px_2px_rgba(255,255,255,1)] border border-white/50 rounded-md -z-10' />
+                                )}
+                                <Link
+                                    href={link.url as LinkProps['href']}
+                                    className={`flex items-center justify-center px-3 py-3 transition hover:text-gray-950 relative ${
+                                        isActive ? 'text-gray-950' : ''
+                                    }`}
+                                >
+                                    {link.name}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
 
             {/* Hamburger Button */}
             <button
@@ -43,28 +69,6 @@ export default function Header() {
                     transition={{ duration: 0.2 }}
                 ></motion.span>
             </button>
-
-            {/* Desktop Navigation */}
-            <nav className='hidden sm:flex fixed top-[1.7rem] left-1/2 h-12 -translate-x-1/2 py-2'>
-                <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:flex-nowrap sm:gap-5'>
-                    {links.map((link) => (
-                        <motion.li
-                            key={link.hash}
-                            className='flex items-center justify-center h-3/4'
-                            initial={{ y: -100, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1 }}
-                            whileHover={{ scale: 1.015 }}
-                        >
-                            <Link
-                                href={link.url as LinkProps['href']}
-                                className='flex items-center justify-center w-full px-3 py-3 transition hover:text-gray-950'
-                            >
-                                {link.name}
-                            </Link>
-                        </motion.li>
-                    ))}
-                </ul>
-            </nav>
 
             {/* Mobile Navigation Overlay */}
             <AnimatePresence>
