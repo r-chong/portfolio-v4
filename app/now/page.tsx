7184eff04c6bad37789df1d3db5a0d81d73d29ab
Reese Chong <dev.reese.chong@gmail.com>
2025-02-16 21:58:31 -0500

    chore: create new nownownow page

---
+++ app/now/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function page() {
+    return (
+        <div>
+            <a
+                href='https://nownownow.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                now page
+            </a>
+        </div>
+    );
+}
