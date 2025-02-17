452f91353bf9a0bbcf3d549a697d99896f9ac76c
Reese Chong <dev.reese.chong@gmail.com>
2025-02-16 23:21:04 -0500

    style: add uw cs webring widget, update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,74 +1,82 @@
 import React from 'react';
-import {
-    FaTwitter,
-    FaInstagram,
-    FaLinkedin,
-    FaGithub,
-    FaRandom,
-} from 'react-icons/fa';
+import { FaXTwitter, FaLinkedin, FaGithub } from 'react-icons/fa6';
 
 export default function Footer() {
     return (
-        <div className='w-full py-4 bg-gray-100 dark:bg-gray-900 border-t-2 border-gray-200 dark:border-gray-800 lg:flex'>
-            <div className='flex justify-center lg:justify-left'>
-                <p className='text-m mx-8 items-center text-[#888] dark:text-gray-400 hidden md:block'>
-                    Reese Chong 2025
-                </p>
-                <div className='flex space-x-4'>
+        <footer className='w-full py-6 bg-gray-100 dark:bg-gray-900 border-t-2 border-gray-200 dark:border-gray-800'>
+            <div className='max-w-4xl mx-auto px-4'>
+                <div className='flex flex-col md:flex-row justify-between items-center gap-4'>
                     {/* <a
-                        href='https://twitter.com/ccbuildspace'
-                        target='_blank'
-                        rel='noopener noreferrer'
+                        href='/blog'
+                        className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200 flex items-center gap-2'
                     >
-                        <FaTwitter
-                            className='text-blue-500 hover:text-blue-700'
-                            size={30}
-                        />
-                    </a>
+                        <span>Random Blog Post</span>
+                        <span className='animate-pulse'>→</span>
+                    </a> */}
 
-                    <a
-                        href='https://www.instagram.com/reese.chong19/'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <FaInstagram
-                            className='text-red-500 hover:text-red-700'
-                            size={30}
-                        />
-                    </a> */}
-                    <a
-                        href='https://www.linkedin.com/in/reesechong'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <FaLinkedin
-                            className='text-blue-700 hover:text-blue-900 dark:text-blue-500 dark:hover:text-blue-400'
-                            size={30}
-                        />
-                    </a>
-                    <a
-                        href='https://github.com/r-chong'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <FaGithub
-                            className='text-gray-900 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300'
-                            size={30}
-                        />
-                    </a>
-                    {/* <a
-                        href='https://github.com/r-chong'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <FaRandom
-                            className='text-gray-900 hover:text-gray-700'
-                            size={30}
-                        />
-                    </a> */}
-                    {/* <em className='items-center'>Random blog post!</em> */}
+                    <div className='flex items-center gap-6'>
+                        <a
+                            href='https://www.linkedin.com/in/reesechong'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='transition-opacity hover:opacity-80'
+                        >
+                            <FaLinkedin
+                                className='text-[#0077B5] dark:text-[#0077B5]'
+                                size={24}
+                            />
+                        </a>
+                        <a
+                            href='https://github.com/r-chong'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='transition-opacity hover:opacity-80'
+                        >
+                            <FaGithub
+                                className='text-gray-900 dark:text-gray-100'
+                                size={24}
+                            />
+                        </a>
+                        <a
+                            href='https://twitter.com/_reesechong'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='transition-opacity hover:opacity-80'
+                        >
+                            <FaXTwitter
+                                className='text-gray-900 dark:text-gray-100'
+                                size={24}
+                            />
+                        </a>
+                    </div>
+
+                    <div className='flex items-center gap-2'>
+                        <a
+                            href='https://cs.uwatering.com/#your-site-here?nav=prev'
+                            className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200'
+                        >
+                            ←
+                        </a>
+                        <a
+                            href='https://cs.uwatering.com/#your-site-here'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <img
+                                src='https://cs.uwatering.com/icon.black.svg'
+                                alt='CS Webring'
+                                className='w-6 h-auto opacity-80 dark:invert'
+                            />
+                        </a>
+                        <a
+                            href='https://cs.uwatering.com/#your-site-here?nav=next'
+                            className='text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200'
+                        >
+                            →
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </footer>
     );
 }
