4c5f95d4d9f3b577d8d8c5d23d93931164997b9d
Reese Chong <dev.reese.chong@gmail.com>
2025-02-16 22:19:42 -0500

    style: update nownownow page

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-export default function page() {
+export default function NowPage() {
     return (
-        <div>
-            why is it not following the layout?
-            <a
-                href='https://nownownow.com/'
-                target='_blank'
-                rel='noopener noreferrer'
-            >
-                now page
-            </a>
-        </div>
+        <article className='flex flex-col items-center w-full'>
+            <div className='container max-w-2xl px-4 mx-auto mt-8 sm:px-6 md:mt-12'>
+                <h1 className='mb-8 text-3xl font-bold'>Now</h1>
+                <div className='prose dark:prose-invert prose-quoteless prose-neutral'>
+                    <p className='text-gray-700 dark:text-gray-300'>
+                        This is what I'm currently focused on.{' '}
+                        <a
+                            href='https://nownownow.com/'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className='text-blue-600 dark:text-blue-400 no-underline hover:underline'
+                        >
+                            Inspired by nownownow.com
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </article>
     );
 }
