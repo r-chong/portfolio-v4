84cca2c31d4547641f742f086d58ef8d038c4a3e
Reese Chong <dev.reese.chong@gmail.com>
2025-02-18 23:18:09 -0500

    feat: implement pitch page + video

---
+++ app/pitch/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import VideoPitch from '@/components/Video';
+
+export default function page() {
+    return (
+        <div>
+            <VideoPitch videoId='eeTDOZHyZgY' />
+        </div>
+    );
+}
