82f988213fee1e3c93a441cc3a68494e30efc9c7
Reese Chong <dev.reese.chong@gmail.com>
2025-03-16 05:37:13 -0400

    chore: center text on project homepage

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,13 +3,15 @@ import Projects from '@/components/Projects';
 
 export const metadata: Metadata = {
     title: 'Projects',
-    description: 'Explore my portfolio of projects and technical work.',
+    description: 'Projects and case studies/technical writeups.',
 };
 
 export default function ProjectsPage() {
     return (
         <div className='max-w-6xl mx-auto py-12 px-4 sm:px-6'>
-            <h1 className='text-3xl font-bold mb-8'>Projects</h1>
+            <h1 className='text-3xl font-bold text-center mb-8'>
+                Projects/Case Studies
+            </h1>
             <Projects displayType='grid' maxIndex={-1} />
         </div>
     );
